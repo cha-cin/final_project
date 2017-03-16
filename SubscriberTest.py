46f7e960f6bf6606925c1e421d13b79be75ffc61
cha-cin <aaiiaann2000@gmail.com>
2017-03-16 13:59:33 +0800

    VERSION_NEW

--- SubscriberTest.py
+++ SubscriberTest.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
  
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("IOT_NEAT_TOPIC01")
+    client.subscribe("123")
  
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
@@ -10,16 +10,16 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("mqtt.phodal.com", 1883, 60)
+client.connect("192.168.2.102", 1883, 60)
 client.loop_forever()
 
 # Publisher.py
-import paho.mqtt.client as mqtt
+#import paho.mqtt.client as mqtt
  
-_g_cst_ToMQTTTopicServerIP = "localhost"
-_g_cst_ToMQTTTopicServerPort = 1883 #port
-_g_cst_MQTTTopicName = "MYTOPIC" #TOPIC name
+#_g_cst_ToMQTTTopicServerIP = "192.168.2.150"
+#_g_cst_ToMQTTTopicServerPort = 1883 #port
+#_g_cst_MQTTTopicName = "123" #TOPIC name
  
-mqttc = mqtt.Client("python_pub")
-mqttc.connect(_g_cst_ToMQTTTopicServerIP, _g_cst_ToMQTTTopicServerPort)
-mqttc.publish(_g_cst_MQTTTopicName, "Hello")
+#mqttc = mqtt.Client("python_pub")
+#mqttc.connect(_g_cst_ToMQTTTopicServerIP, _g_cst_ToMQTTTopicServerPort)
+#mqttc.publish(_g_cst_MQTTTopicName, "Hello")
