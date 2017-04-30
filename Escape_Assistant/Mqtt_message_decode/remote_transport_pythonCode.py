2f39cf5a948dd49a82ca1b1c23ccd98a0e0c493b
cha-cin <aaiiaann2000@gmail.com>
2017-04-30 15:42:55 +0800

    MicroPython_Mqtt_Encode

---
+++ Escape_Assistant/Mqtt_message_decode/remote_transport_pythonCode.py
@@ -0,0 +1,85 @@
+from umqtt.simple import MQTTClient
+import ubinascii
+import machine
+import micropython
+
+
+def do_connect():
+    import network
+
+    SSID = 'Mster'
+    PASSWORD = 'rty91119'
+
+    sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    if ap_if.active():
+        ap_if.active(False)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(SSID, PASSWORD)
+        while not sta_if.isconnected():
+            pass
+    print('Network configuration:', sta_if.ifconfig())
+
+
+
+# Default MQTT server to connect to
+SERVER = "192.168.2.200"
+CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+TOPIC = b"fire1"
+PORT_NO = 1883     #change to mqtt port no.
+USERNAME = "pi"
+PASSWORD = "raspberry"
+
+
+state = 0
+
+def sub_cb(topic, msg):
+    global state
+    #print((topic, msg))
+    #msg is bytes
+    msg_decode=str(msg,'utf-8')
+    print(msg_decode)
+    #print(type(msg_decode))
+    
+    str2 = ''
+    i=0
+    while i < len(msg_decode):
+        if msg_decode[i] is '\\':
+            if msg_decode[i+1] is 'n':
+                str2 = str2+'\n'
+                i=i+2
+            elif msg_decode[i+1] is 't':
+                str2 = str2+'\t'
+                i=i+2
+            else:
+                i=i+1
+        else:
+            str2 = str2+msg_decode[i]
+            i=i+1
+        print(str2)
+        f = open('new_excute.py', 'w')
+        f.write(str2)
+        f.close()
+    
+def main(server=SERVER):
+    c = MQTTClient(CLIENT_ID, server, PORT_NO, USERNAME, PASSWORD, 60)
+    # Subscribed messages will be delivered to this callback
+    c.set_callback(sub_cb)
+    c.connect()
+    c.subscribe(TOPIC)
+    print("Connected to %s, subscribed to %s topic" % (server, TOPIC))
+
+    try:
+        while 1:
+            #micropython.mem_info()
+            c.wait_msg()
+    finally:
+        c.disconnect()
+
+if __name__ == '__main__':
+#    load_config()
+#    setup_pins()
+    do_connect()
+    main()
