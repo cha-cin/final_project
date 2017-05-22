07e86e79986771714e0f760698c1e13af33bfa77
cha-cin <aaiiaann2000@gmail.com>
2017-05-22 18:41:08 +0800

    homeassistant_mqtt

--- Escape_Assistant/Mqtt_message_decode/homeassistant_encode.py
+++ Escape_Assistant/Mqtt_message_decode/homeassistant_encode.py
@@ -1,13 +1,26 @@
 import paho.mqtt.client as mqtt
-
 def on_connect(client, userdata, flags, rc):
-    for i in range(3):
+    for i in range(6):
+
     #print("Connected with result code "+str(rc))
     #client.subscribe("fire1")
     #client.subscribe("fire2")
     #client.subscribe("fire3")
         client.subscribe("fire"+str(i))
+
 def on_message(client, userdata, msg):
+    if msg.topic == 'fire1':
+        print('YEE_1')
+    if msg.topic == 'fire2':
+        print('YEE_1')
+    if msg.topic == 'fire3':
+        print('YEE_1')
+    if msg.topic == 'fire4':
+        print('YEE_1')
+    if msg.topic == 'fire5':
+        print('YEE_1')
+    if msg.topic == 'fire6':
+        print('YEE_1')                    
     #print(msg.topic+" "+str(msg.payload))
     want=str(msg.payload)[2:(len(str(msg.payload))-1)]
     str2 = ''
@@ -31,19 +44,21 @@ def on_message(client, userdata, msg):
             str2 = str2+want[i]
             i=i+1
     print(str2)
-        
     
     #want[i] = want[i]+'\t'
     #print(want)
     
     #print(str2.decode('utf-8'))
-    f = open('new_excute.py', 'w')
+    #f = open('new_excute.py', 'w')
+    f = open('sensor_status.txt', 'r+')
+    f.seek(0,0)
     f.write(str2)
     #print(bytes.decode(want))
     f.close()
 
-
-
+f = open('sensor_status.txt', 'w')
+f.write("000000000000")
+f.close()
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
---
+++ Escape_Assistant/Mqtt_message_decode/homeassistant_mqtt_publish.py
@@ -0,0 +1,54 @@
+from umqtt.simple import MQTTClient
+import ubinascii
+import machine
+import micropython
+
+
+def do_connect():
+    import network
+
+    SSID = 'Mster'
+    PASSWORD = 'rty91119'
+
+    sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    if ap_if.active():
+        ap_if.active(False)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(SSID, PASSWORD)
+        while not sta_if.isconnected():
+            pass
+    print('Network configuration:', sta_if.ifconfig())
+
+
+
+# Default MQTT server to connect to
+SERVER = "192.168.2.200"
+CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+TOPIC = b"fire5"
+PORT_NO = 1883     #change to mqtt port no.
+USERNAME = "pi"
+PASSWORD = "raspberry"
+
+
+state = 0
+
+    
+def main(server=SERVER):
+    c = MQTTClient(CLIENT_ID, server, PORT_NO, USERNAME, PASSWORD, 60)
+    # Subscribed messages will be delivered to this callback
+    
+    c.connect()
+    c.publish(TOPIC,'12')
+    print("Connected to %s, publish to %s topic" % (server, TOPIC))
+
+    
+    c.disconnect()
+
+if __name__ == '__main__':
+#    load_config()
+#    setup_pins()
+    do_connect()
+    main()
