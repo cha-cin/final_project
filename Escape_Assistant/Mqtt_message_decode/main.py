49cf4de2f3c17d8930d283fb9e4cc8e7b6ca7aa8
cha-cin <aaiiaann2000@gmail.com>
2017-09-19 19:10:58 +0800

    version_finall
    can Identification which fire_sensor is fire

--- Escape_Assistant/Mqtt_message_decode/main.py
+++ Escape_Assistant/Mqtt_message_decode/main.py
@@ -1,10 +1,11 @@
-from umqtt.simple import MQTTClient
+from umqtt.simple import MQTTClient 
 import ubinascii
 import machine
 import micropython
 import time
 import execute
 import decode
+
 
 
 def do_connect():
@@ -50,29 +51,50 @@ def sub_cb(topic, msg):
     decode.de(msg_decode)
 
 def main(server=SERVER):
-    c = MQTTClient(CLIENT_ID, server, PORT_NO, USERNAME, PASSWORD, 60)
+    c = MQTTClient (CLIENT_ID, server, PORT_NO, USERNAME, PASSWORD, 60)
     # Subscribed messages will be delivered to this callback
     c.set_callback(sub_cb)
     c.connect()
-    c.publish(TOPIC_Publish,'1')
+    
     c.subscribe(TOPIC)
     print("Connected to %s, subscribed to %s topic" % (server, TOPIC))
     execute.open()
-    
+    change=1
     try:
         while 1:
             #micropython.mem_info()
+            led =machine.Pin(9, machine.Pin.IN)
+            led2 =machine.Pin(12, machine.Pin.IN)
+            if led.value() is 0 and led2.value() is 1:
+                c.publish(TOPIC_Publish,'10')
+                print("ok!")
+            elif led.value() is 0 and led2.value() is 0:
+                c.publish(TOPIC_Publish,'11')
+                print("ok!")
+            elif led.value() is 1 and led2.value() is 0:
+                c.publish(TOPIC_Publish,'01')
+            elif led.value() is 0:
+                if led2.value() is 0:
+                    c.publish(TOPIC_Publish,'11')
+                    print("ok!")
+            elif led2.value() is 0:
+                if led.value() is 0:
+                    c.publish(TOPIC_Publish,'11')
+                    print("ok!")
             c.wait_msg()
             print("finish!!!")
             time.sleep(1)
             machine.reset()
+            
 
     finally:
         print("finish!!!")
         time.sleep(1)
         machine.reset()
+
     c.disconnect()
 
+    
     #c.disconnect()
 if __name__ == '__main__':
 #    load_config()
