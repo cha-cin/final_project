14792c351f71f01ff06c0760acce5bc3b3125009
cha-cin <aaiiaann2000@gmail.com>
2017-10-11 20:13:18 +0800

    final version

--- Escape_Assistant/Mqtt_message_decode/main.py
+++ Escape_Assistant/Mqtt_message_decode/main.py
@@ -3,9 +3,9 @@ import ubinascii
 import machine
 import micropython
 import time
-import execute
 import decode
-
+import execute
+import sys
 
 
 def do_connect():
@@ -33,6 +33,7 @@ SERVER = "192.168.2.200"
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 TOPIC = b"thirteen"
 TOPIC_Publish = b"One"
+TOPIC_Publish2 = b"Rat"
 PORT_NO = 1883     #change to mqtt port no.
 USERNAME = "pi"
 PASSWORD = "raspberry"
@@ -47,52 +48,54 @@ def sub_cb(topic, msg):
     msg_decode=str(msg,'utf-8')
     print(msg_decode)
     #print(type(msg_decode))
-    
     decode.de(msg_decode)
 
 def main(server=SERVER):
-    c = MQTTClient (CLIENT_ID, server, PORT_NO, USERNAME, PASSWORD, 60)
+    #c = MQTTClient (CLIENT_ID, server, PORT_NO, USERNAME, PASSWORD, 60)
     # Subscribed messages will be delivered to this callback
-    c.set_callback(sub_cb)
-    c.connect()
-    
-    c.subscribe(TOPIC)
-    print("Connected to %s, subscribed to %s topic" % (server, TOPIC))
-    execute.open()
-    change=1
-    try:
-        while 1:
-            #micropython.mem_info()
-            led =machine.Pin(9, machine.Pin.IN)
-            led2 =machine.Pin(12, machine.Pin.IN)
-            if led.value() is 0 and led2.value() is 1:
-                c.publish(TOPIC_Publish,'10')
-                print("ok!")
-            elif led.value() is 0 and led2.value() is 0:
-                c.publish(TOPIC_Publish,'11')
-                print("ok!")
-            elif led.value() is 1 and led2.value() is 0:
-                c.publish(TOPIC_Publish,'01')
-            elif led.value() is 0:
-                if led2.value() is 0:
-                    c.publish(TOPIC_Publish,'11')
-                    print("ok!")
-            elif led2.value() is 0:
-                if led.value() is 0:
-                    c.publish(TOPIC_Publish,'11')
-                    print("ok!")
-            c.wait_msg()
-            print("finish!!!")
-            time.sleep(1)
-            machine.reset()
-            
+    #c.set_callback(sub_cb)
+    #c.connect()
+    #c.subscribe(TOPIC)
+    #print("Connected to %s, subscribed to %s topic" % (server, TOPIC))
+    #change=1
+    #try:
+    while 1:
+        c = MQTTClient (CLIENT_ID, server, PORT_NO, USERNAME, PASSWORD, 60)
+        # Subscribed messages will be delivered to this callback
+        c.set_callback(sub_cb)
+        c.connect()
+        c.subscribe(TOPIC)
+        print("Connected to %s, subscribed to %s topic" % (server, TOPIC))
+        change=1
+        #micropython.mem_info()
+        led =machine.Pin(9, machine.Pin.IN)
+        led2 =machine.Pin(12, machine.Pin.IN)
+        if led.value() is 0:
+            c.publish(TOPIC_Publish,'1')
+            print("SD2 ok!")
+        if led2.value() is 0:
+            c.publish(TOPIC_Publish2,'1')
+            print("D6 ok!")
+        c.wait_msg()
+        del sys.modules['execute']
+        import execute
+        execute.open()
+        f = open('execute.py', 'r')
+        sss = f.read()
+        print(sss)
+        f.close()
+        print("finish")
+        c.disconnect()
+            #time.sleep(1)
+            #machine.reset()
+#鼠：Rat，牛：Ox，虎：Tiger，兔：Hare，龙：Dragon ，蛇：Snake，马：Horse，羊：Sheep，猴：Monkey，鸡：Cock，狗：Dog，猪：Boar           
 
-    finally:
-        print("finish!!!")
-        time.sleep(1)
-        machine.reset()
+    #finally:
+        #print("finish!!!")
+        #time.sleep(1)
+        #machine.reset()
 
-    c.disconnect()
+    #c.disconnect()
 
     
     #c.disconnect()
